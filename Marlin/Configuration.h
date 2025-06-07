df8a9de2136fc32a69afc54f687abb27a2531f54
meganukebmp <kristiana2000@abv.bg>
2025-06-07 21:31:04 +0200

    PID tune

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -714,9 +714,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  19.54
-    #define DEFAULT_Ki  1.69
-    #define DEFAULT_Kd  54.63
+    #define DEFAULT_Kp 37.81
+    #define DEFAULT_Ki 7.39
+    #define DEFAULT_Kd 48.40
   #endif
 #else
   #define BANG_MAX 255    // Limit hotend current while in bang-bang mode; 255=full current
@@ -809,9 +809,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 113.22
-  #define DEFAULT_bedKi 20.55
-  #define DEFAULT_bedKd 415.79
+  #define DEFAULT_bedKp 104.37
+  #define DEFAULT_bedKi 19.47
+  #define DEFAULT_bedKd 372.95
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #else
